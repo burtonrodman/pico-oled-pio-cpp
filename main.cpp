341f5f21bd67801bb9b6c680a8837464641c3a32
Burton Rodman <burtonrodman@hotmail.com>
2023-02-12 23:54:27 -0500

    oled/i2c working again

--- main.cpp
+++ main.cpp
@@ -1,89 +1,12 @@
-#include <stdio.h>
-
-#include "I2C.h"
-#include "PioI2C.h"
-#include "pio_i2c.h"
-#include "i2c.pio.h"
+#include <cstdio>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/i2c.h"
 
 #include "OLED.h"
 #include "font/Cherry_Cream_Soda_Regular_16.h"
 
-#include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-
-#include "MixerModel.h"
-
-#include "tusb.h"
-#include "class/midi/midi_host.h"
-
-static uint8_t midi_dev_addr = 0;
-
-int main() {
-    board_init();
-    stdio_init_all();
-    tusb_init();
-    
-    if (cyw43_arch_init()) 
-    {
-        printf("Wifi init failed");
-        return -1;
-    }
-
-    // pause to allow starting PuTTY
-    // cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-    // sleep_ms(10000);
-    // cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-    // sleep_ms(50);
-
-    PioI2C pio(17, 16, 400 * 1000, pio0);
-    I2C* i2c = &pio;
-    OLED oled(128, 64, false, i2c);
-
-    printf("initializing state");
-    MixerModel mixer();
-
-    printf("waiting for MIDI events");
-    while (1) {
-        tuh_task();
-
-        blink_led();
-        bool connected = midi_dev_addr != 0 && tuh_midi_configured(midi_dev_addr);
-
-        if (connected)
-            tuh_midi_stream_flush(midi_dev_addr);
-        poll_usb_rx(connected);
-
-        
-    }
-
-    return 0;
-}
-
-void drawChannelOled()
-{
-    oled.drawCircle(100, 16, 14);
-    oled.drawFilledCircle(100, 16, 10);
-    // Print defaule font string
-    uint8_t string1[] = "sup";
-    oled.print(0, 35, string1);
-    // Print custom font string
-    oled.setFont(&Cherry_Cream_Soda_Regular_16);
-    uint8_t string2[] = "yo!";
-    oled.print(60, 32, string2);
-    // Draw a line
-    oled.drawFastHLine(0, 60, 128);
-    oled.show();
-
-}
-
-void drawMasterOled() {
-
-}
-
-void drawSystemOled() {
-
-}
-
+#include "HardwareI2C.h"
 
 static void blink_led(void)
 {
@@ -101,76 +24,46 @@ static void blink_led(void)
     }
 }
 
-static void poll_usb_rx(bool connected)
+void drawChannelOled(OLED* oled)
 {
-    // device must be attached and have at least one endpoint ready to receive a message
-    if (!connected || tuh_midih_get_num_rx_cables(midi_dev_addr) < 1)
+    printf("in drawChannelOled\n");
+    oled->drawCircle(100, 16, 14);
+    oled->drawFilledCircle(100, 16, 10);
+    // Print defaule font string
+    uint8_t string1[] = "sup";
+    oled->print(0, 35, string1);
+    // Print custom font string
+    oled->setFont(&Cherry_Cream_Soda_Regular_16);
+    uint8_t string2[] = "yo!";
+    oled->print(60, 32, string2);
+    // Draw a line
+    oled->drawFastHLine(0, 60, 128);
+    oled->show();
+}
+
+int main() {
+    stdio_init_all();
+    
+    if (cyw43_arch_init()) 
     {
-        return;
+        printf("Wifi init failed\n");
+        return -1;
     }
-    tuh_midi_read_poll(midi_dev_addr);
+
+    // pause to allow starting PuTTY
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+    sleep_ms(9000);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+    sleep_ms(50);
+
+    printf("initializing displays\n");
+
+    HardwareI2C hw(17, 16, 400 * 1000, i2c0);
+    I2C* i2c = &hw;
+    OLED oled(128, 64, false, i2c);
+
+    drawChannelOled(&oled);
+
+    while (1) { blink_led(); }
 }
 
-//--------------------------------------------------------------------+
-// TinyUSB Callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device with hid interface is mounted
-// Report descriptor is also available for use. tuh_hid_parse_report_descriptor()
-// can be used to parse common/simple enough descriptor.
-// Note: if report descriptor length > CFG_TUH_ENUMERATION_BUFSIZE, it will be skipped
-// therefore report_desc = NULL, desc_len = 0
-void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t num_cables_rx, uint16_t num_cables_tx)
-{
-  printf("MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u cables\r\n",
-      dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
-
-  if (midi_dev_addr == 0) {
-    // then no MIDI device is currently connected
-    midi_dev_addr = dev_addr;
-  }
-  else {
-    printf("A different USB MIDI Device is already connected.\r\nOnly one device at a time is supported in this program\r\nDevice is disabled\r\n");
-  }
-}
-
-// Invoked when device with hid interface is un-mounted
-void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance)
-{
-  if (dev_addr == midi_dev_addr) {
-    midi_dev_addr = 0;
-    printf("MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
-  }
-  else {
-    printf("Unused MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
-  }
-}
-
-void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets)
-{
-    if (midi_dev_addr == dev_addr)
-    {
-        if (num_packets != 0)
-        {
-            uint8_t cable_num;
-            uint8_t buffer[48];
-            while (1) {
-                uint32_t bytes_read = tuh_midi_stream_read(dev_addr, &cable_num, buffer, sizeof(buffer));
-                if (bytes_read == 0)
-                    return;
-                if (cable_num == 0) {
-                    printf("received: ");
-                    for (int i = 0; i < bytes_read; i++) {
-                        printf("%02x ", buffer[i]);
-                    }
-                    printf("\n");
-                }
-            }
-        }
-    }
-}
-
-void tuh_midi_tx_cb(uint8_t dev_addr)
-{
-    (void)dev_addr;
-}
